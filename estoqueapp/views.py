6288df111820d0a7bcf185f3703e1ed0e0af0328
David Santana <david.santana98@hotmail.com>
2023-04-25 00:09:48 -0300

    delete e ajax

--- estoqueapp/views.py
+++ estoqueapp/views.py
@@ -36,3 +36,8 @@ def update(request, pk):
     if form.is_valid():
         form.save()
         return redirect('home')
+
+def delete(request, pk):
+    db = Itens.objects.get(pk=pk)
+    db.delete()
+    return redirect(home)
